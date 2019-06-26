e7dbd87c761aec3f428075d29561ae374806aa2e
dokechin <ttatsumi@ra2.so-net.ne.jp>
2019-06-27 07:49:40 +0900

    remove require

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const clova = require('../clova');
-const bot = require('../bot');
-const api = require('../api');
-const setting = require('../setting');
 const router = express.Router();
 
 router.post(`/clova`, clova);
