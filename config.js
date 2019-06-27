dcdcced01fb03716067a981e0524f03b024e1530
dokechin <ttatsumi@ra2.so-net.ne.jp>
2019-06-27 20:58:07 +0900

    change port

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SERVER_PORT: process.env.SERVER_PORT || 3000,
+  SERVER_PORT: process.env.SERVER_PORT || 3001,
   DOMAIN: 'https://pelvic.dokechin.com/clova',
   ExtensionId: 'com.dokechin.pelvic',
   DEBUG: process.env.DEBUG || false,
